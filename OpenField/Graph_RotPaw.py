2bd16ab7fb13beb5814b0164b8c2578ab37fd0b1
Meshva Desai <meshva03@gmail.com>
2021-08-19 15:45:15 -0400

    line +scatter + bar graph

--- OpenField/Graph_RotPaw.py
+++ OpenField/Graph_RotPaw.py
@@ -4,13 +4,13 @@ import pandas as pd
 
 def get_group(name):
     if group_1.count(name) > 0:
-        return None
-    elif group_2.count(name) > 0:
         return 1
-    elif group_3.count(name) > 0:
-        return None
-    elif group_4.count(name) > 0:
+    elif group_2.count(name) > 0:
         return 2
+    elif group_3.count(name) > 0:
+        return 3
+    elif group_4.count(name) > 0:
+        return 4
 
 def sort_means(names, means):
     sorted_means = [None, None, None, None]
@@ -61,7 +61,7 @@ def sorted_ratios(df, df_2):
     return sorted_ratios
 
 def plot_bar(df, num_groups, ax, color, bar_pos, sorted_ratios=None):
-    if df != None:
+    if sorted_ratios == None:
         names = df['name']
         means = df['adjusted mean']
         sorted_means = sort_means(names, means)
@@ -81,7 +81,41 @@ def plot_bar(df, num_groups, ax, color, bar_pos, sorted_ratios=None):
         print(avg)
 
         #plot avg line
-        ax.hlines(avg, xmin = bar_pos[i]-0.2, xmax = bar_pos[i]+0.2, color='blue')
+        ax.hlines(avg, xmin = bar_pos[i]-0.2, xmax = bar_pos[i]+0.2, color='k')
+
+    ax.axhline(1, linestyle='--', color='gray') #x-axis at y=0
+
+def plot_bar_connected(df, df_2, ax):
+    names = df['name']
+    means = df['adjusted mean']
+
+    names_2 = df_2['name']
+    means_2 = df_2['adjusted mean']
+
+    for i in range (0, len(names_2)):
+        n = names_2[i]
+        group = get_group(n)
+
+        if group != None:
+            j = names.values.tolist().index(n)
+
+            #jitter = np.random.uniform(low=-0.05, high=+0.05)
+
+            x = group - 0.1
+            x_2 = group + 0.1
+
+            y = np.abs(means[j])
+            y_2 = np.abs(means_2[i])
+
+            ax.plot(x, y, 'ro')
+            ax.plot(x_2, y_2, 'bo')
+
+            #find slope of line
+            m = (y_2 - y) / (x_2 - x)
+
+            #plot line between two
+            ax.plot([x, x_2], [y, y_2], color='k')
+
 
 
 group_1 = ['PD5-01', 'PD5-02', 'PD5-03', 'PD5-13', 'PD5-14']
@@ -91,25 +125,25 @@ group_4 = ['PD5-04', 'PD5-08', 'PD5-15', 'PD5-06', 'PD06-03', 'PD06-04', 'PD06-0
 
 basepath = 'C:/Users/HS student/Desktop/Behavioural Analysis/'
 
-df = pd.read_csv(basepath + 'Test_1/Rotation Test.csv')
-df_2 = pd.read_csv(basepath + 'Test_2/Rotation Test.csv')
+df = pd.read_csv(basepath + 'Test_1/Cylinder Test.csv')
+df_2 = pd.read_csv(basepath + 'Test_2/Cylinder Test.csv')
 
 fig, ax = plt.subplots(1,1)
 
-#plot_bar(df, 2, ax, 'r', [1,3]) #before
-#plot_bar(df_2, 2, ax, 'b', [2,4]) #after
+#plot_bar(df, 4, ax, 'r', [1,3,5,7]) #before
+#plot_bar(df_2, 4, ax, 'b', [2,4,6,8]) #after
 
-ratios = sorted_ratios(df, df_2)
-plot_bar(None, 2, ax, 'r', [1,2], ratios)
+#ratios = sorted_ratios(df, df_2)
+#plot_bar(None, 2, ax, 'r', [1,2], ratios)
 
-ax.axhline(1, linestyle='--', color='gray') #x-axis at y=0
+plot_bar_connected(df, df_2, ax)
 
-ax.set_xticks([1,2])
+ax.set_xticks([1,2,3,4])
 
 #change depending on slides
-ax.set_xticklabels(['Control', 'FUS + MB'])
+ax.set_xticklabels(['1', '2', '3', '4'])
 
-plt.savefig(basepath + 'rotation_FUSvControl' + '.png')
+plt.savefig(basepath + 'cylinder_lineplot' + '.png')
 print('saved')
 
 plt.show()
